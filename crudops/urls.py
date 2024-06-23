f6439da0d7353eb0862ff9e5b4f74346092b2f00
mumiak-mitch <akoth0mitch@gmail.com>
2024-06-23 23:02:06 +0300

    small UI adjustments

--- crudops/urls.py
+++ crudops/urls.py
@@ -2,8 +2,8 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.crud_form, name="crudform"),
+    path('', views.crud_list, name="crudlist"),
+    path('form/', views.crud_form, name="crudform"),
     path('<int:id>/', views.crud_form, name="updateform"),
-    path('list/', views.crud_list, name="crudlist"),
     path('delete/<int:id>/', views.crud_delete, name="cruddelete"),
 ]
